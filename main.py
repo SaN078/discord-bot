93dac71a16df854ff37f678cad5737e350540dfd
SaN078 <sangeetraut29@gmail.com>
2023-05-26 06:49:10 +0545

    First Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author ==  bot.user:
+        return
+    
+    if message.content.startswith("hello"):
+        await message.channel.send("Hey~~")
+    
+    if message.content.startswith("oo"):
+        await message.channel.send("O.O")
+
+
+
+
+#@bot.command
+#async def wave():
+    
+
+
+bot.run('MTAzMzMyMTMwMjY3ODgzNTI1MQ.GIiUsq.-n77ei3mFta6qIUc_EecanqnrRtjzleowswDvQ')
